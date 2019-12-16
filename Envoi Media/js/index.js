ba7f6cdd792c22975d67cf2429eb38e0f51e79a7
torndorff5 <torndorff5@gmail.com>
2019-12-16 12:28:55 -0700

    Adding to Portfolio

---
+++ Envoi Media/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $("#influence-button").click(function () {
+    window.location.href ="influencer/influencer-signup.html";
+  });
+  $("#business-button").click(function () {
+    window.location.href ="business/business-startfreetrial.html";
+  });
+});
